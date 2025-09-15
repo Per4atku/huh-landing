97895eb3e539cc9d55acd6580d8b5b4c3427d401
Per4atku <artcchi@gmail.com>
2025-09-15 18:53:49 +1000

    Init

---
+++ src/components/MaxWidthWrapper.tsx
@@ -0,0 +1,34 @@
+import React, { ReactNode } from "react";
+
+interface MaxWidthWrapperProps {
+  children: ReactNode;
+  className?: string;
+  maxWidth?: string | number;
+  id?: string;
+}
+
+const MaxWidthWrapper: React.FC<MaxWidthWrapperProps> = ({
+  children,
+  className = "",
+  maxWidth = "1200px",
+  id,
+}) => {
+  return (
+    <div
+      id={id}
+      className={className}
+      style={{
+        maxWidth,
+        paddingLeft: "1rem",
+        paddingRight: "1rem",
+        marginLeft: "auto",
+        marginRight: "auto",
+        width: "100%",
+      }}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default MaxWidthWrapper;
