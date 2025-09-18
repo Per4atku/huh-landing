4903fdb1f1e7d68726c6fcee6649af11f0fe548f
Per4atku <artcchi@gmail.com>
2025-09-18 19:46:38 +1000

    [+] Demo section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+import Demo from "@/components/sections/Demo";
 import Hero from "@/components/sections/Hero";
 
 export default function Home() {
   return (
     <main>
-      <MaxWidthWrapper maxWidth={1200}>
+      <MaxWidthWrapper maxWidth={8000}>
         <Hero />
+        <MaxWidthWrapper maxWidth={1200}>
+          <Demo />
+        </MaxWidthWrapper>
       </MaxWidthWrapper>
     </main>
   );
---
+++ src/components/sections/Demo.tsx
@@ -0,0 +1,36 @@
+import Image from "next/image";
+
+const Demo = () => {
+  return (
+    <div className="relative h-screen flex flex-col items-center justify-center gap-14">
+      {/* Heading + small dash image */}
+      <div className="relative">
+        <h2 className="text-3xl text-center sm:text-5xl">
+          Make your life way
+          <br /> more <span className="font-bold">productive</span>
+        </h2>
+        <div className="absolute right-0 sm:w-[241px] sm:right-5 ">
+          <Image
+            src="/dash.png"
+            width={176}
+            height={25}
+            alt="dash"
+            className="w-auto h-auto sm:w-[241px]"
+          />
+        </div>
+      </div>
+
+      {/* App demo image */}
+      <div className="relative w-[90%] max-w-[1228px] aspect-[1228/745]">
+        <Image
+          src="/app-demo.png"
+          fill
+          style={{ objectFit: "contain" }}
+          alt="Demo of the App"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Demo;
